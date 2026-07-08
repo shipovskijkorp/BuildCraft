dd6e93c9a3ac1653a3aab4f8c4b2c694c591703c
ShipovskijKorp <shipovskijkorparation@gmail.com>
2026-07-08 22:18:15 +0300

    fixed dock station #24

--- src/main/java/ct/buildcraft/robotics/BCRoboticsModels.java
+++ src/main/java/ct/buildcraft/robotics/BCRoboticsModels.java
@@ -3,13 +3,16 @@ package ct.buildcraft.robotics;
 import ct.buildcraft.api.transport.pipe.PipeApiClient;
 import ct.buildcraft.api.transport.pluggable.IPluggableStaticBaker;
 import ct.buildcraft.lib.client.model.ModelHolderStatic;
-import ct.buildcraft.lib.client.model.plug.PlugBakerSimple;
+import ct.buildcraft.robotics.client.model.plug.PlugBakerRobotStation;
 import ct.buildcraft.robotics.client.model.key.KeyRobotStation;
 import net.minecraftforge.client.event.ModelEvent.BakingCompleted;
 
 public final class BCRoboticsModels {
-    public static final ModelHolderStatic ROBOT_STATION = new ModelHolderStatic("buildcraftrobotics:pluggables/robot_station");
-    public static final IPluggableStaticBaker<KeyRobotStation> BAKER_ROBOT_STATION = new PlugBakerSimple<>(ROBOT_STATION::getCutoutQuads);
+    public static final ModelHolderStatic ROBOT_STATION_AVAILABLE = new ModelHolderStatic("buildcraftrobotics:pluggables/robot_station");
+    public static final ModelHolderStatic ROBOT_STATION_RESERVED = new ModelHolderStatic("buildcraftrobotics:pluggables/robot_station_reserved");
+    public static final ModelHolderStatic ROBOT_STATION_LINKED = new ModelHolderStatic("buildcraftrobotics:pluggables/robot_station_linked");
+    public static final IPluggableStaticBaker<KeyRobotStation> BAKER_ROBOT_STATION = new PlugBakerRobotStation(
+            ROBOT_STATION_AVAILABLE, ROBOT_STATION_RESERVED, ROBOT_STATION_LINKED);
 
     private BCRoboticsModels() {
     }
